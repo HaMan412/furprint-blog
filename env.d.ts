c89a2c4f101478538b422d8e3e9e5062a5699f9c
Man Ha <1805659145@qq.com>
2025-10-18 22:54:41 +0800

    Add recent games

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="astro/client" />
+
+declare module '@pagefind/default-ui' {
+  export class PagefindUI {
+    constructor(opts: any);
+    triggerSearch(term: string): void;
+  }
+}
